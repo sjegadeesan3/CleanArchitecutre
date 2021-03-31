11f170b1ed54a540c2515de6a6206a5071244c3d
JEGADEESAN <sjegadeesan3@gmail.com>
2021-03-31 23:46:04 +0800

    View binding for people adapter

--- app/src/main/java/com/jegadeesan/myapplication/presentation/adapter/PeopleAdapter.kt
+++ app/src/main/java/com/jegadeesan/myapplication/presentation/adapter/PeopleAdapter.kt
@@ -1,30 +1,27 @@
 package com.jegadeesan.myapplication.presentation.adapter
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import com.jegadeesan.myapplication.R
 import com.jegadeesan.myapplication.data.model.People
+import com.jegadeesan.myapplication.databinding.PeopleLineItemBinding
 
 class PeopleAdapter(private val peopleList: List<People>):  RecyclerView.Adapter<PeopleAdapter.PeopleViewHolder>(){
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PeopleViewHolder {
-        val view = LayoutInflater.from(parent.context)
-            .inflate(R.layout.people_line_item, parent, false)
-        return PeopleViewHolder(view)
+        val binding = PeopleLineItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return PeopleViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: PeopleViewHolder, position: Int) {
-        holder.textView.text = peopleList[position].name
+        holder.binding.peopleName.text = peopleList[position].name
     }
 
     override fun getItemCount(): Int =
         peopleList.size
 
-    class PeopleViewHolder(view: View): RecyclerView.ViewHolder(view) {
-        val textView: TextView = view.findViewById(R.id.people_name)
+    class PeopleViewHolder(peopleLineItemBinding: PeopleLineItemBinding): RecyclerView.ViewHolder(peopleLineItemBinding.root) {
+        val binding: PeopleLineItemBinding = peopleLineItemBinding
     }
 
 }
